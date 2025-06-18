500ae63175afa492109c53f12b2d6eda56119f3d
ki <kre1238@gmail.com>
2025-06-18 19:27:47 +0300

    fix from eslint

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync'
 
 export default (getName) => {
-    let userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+  let userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
 }
