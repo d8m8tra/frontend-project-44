89fa4268f94153cf4a46b1c565d34aaa76312a74
ki <kre1238@gmail.com>
2025-06-24 16:21:54 +0300

    add index.js, gcd.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import brainGames from '../bin/brain-games.js'
+import getName from '../src/cli.js'
+import evenGame from '../src/games/calc.js'
+import calc from '../src/games/calc.js'
+
+// скорее всего, в одном из шагов попросят объединить игры
+brainGames()
+const userName = getName()
