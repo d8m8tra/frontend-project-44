4ee43eb6912abe2949518bec69aba5804dc46684
ki <kre1238@gmail.com>
2025-06-24 13:25:38 +0300

    fix brain-calc

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 import brainGames from './brain-games.js'
-import evenGame from '../src/games/calc.js'
+import calc from '../src/games/calc.js'
 import getName from '../src/cli.js'
 
 brainGames()
 const userName = getName()
-evenGame(userName)
+calc(userName)
