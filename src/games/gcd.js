e5bd2a8f3bfd67eb9b4035822e0f80308bd04b7e
ki <kre1238@gmail.com>
2025-07-01 19:24:12 +0300

    fix gcd.ja

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -7,8 +7,8 @@ const textForGame = 'Find the greatest common divisor of given numbers.'
 // вопрос юзеру и правильный ответ
 const usrQuestionAndCorrectAnsw = () => {
   let correctAnsw = 0
-  let firstNumber = randomNumber()
-  let secondNumber = randomNumber()
+  let firstNumber = randomNumber() + 1
+  let secondNumber = randomNumber() + 1
   let usrQuestion = `${firstNumber} ${secondNumber}`
 
   while (secondNumber !== 0) {
